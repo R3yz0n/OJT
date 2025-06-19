7120c60d668dae38a19b2affac55203fe60e918c
r3yz0n <reyzon.thapa2020@gmail.com>
2025-06-19 12:39:28 +0545

    Add index.html and script.js for day3; implement JavaScript data types and examples Add que1.js, que2.js, que3.js, que4.js, and que5.js for day4; demonstrate variable scope and hoisting in JavaScript

---
+++ javascript/day3/script.js
@@ -0,0 +1,13 @@
+let isMarried = false;
+let name = "John Doe";
+let age = 30;
+
+let person = {
+  meroname: "John Doe",
+  umer: 30,
+  isStudent: true,
+};
+
+console.log(isMarried); // false
+console.log(name); // John Doe
+console.log(age); // 30
---
+++ javascript/day4/que5.js
@@ -0,0 +1,2 @@
+console.log(x); // undefined ❗
+var x = 5;
