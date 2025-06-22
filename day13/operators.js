c2a0242cd81b6cae13ef61caaeed186f4500cb09
r3yz0n <reyzon.thapa2020@gmail.com>
2025-06-22 13:13:18 +0545

    Add operators.js to demonstrate relational and logical operators with voting eligibility example

---
+++ day13/operators.js
@@ -0,0 +1,17 @@
+// Relational Operator
+
+// let firstNumber = 5;
+// let secondNumber = 3;
+// console.log(5 === "5");
+
+// Person should be a citizen or Nepal
+// Should be greater than 18
+
+let citzen = "USA";
+let age = 21;
+
+// Logical AND
+if (citzen === "Nepal" && age >= 18) {
+  console.log;
+  ("You are eligible to vote in Nepal.");
+}
