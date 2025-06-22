a58e16a1706bc44039ea20f2754230087bd05306
r3yz0n <reyzon.thapa2020@gmail.com>
2025-06-22 14:23:02 +0545

    Add function.js to implement basic functions and demonstrate console logging

---
+++ day13/function.js
@@ -0,0 +1,20 @@
+// a block that calculates the sum of two numbers
+
+// functino body, here sayHello is fn name
+function sayHello() {
+  console.log("Hello, World!");
+  //
+  //
+}
+console.log("Hi");
+
+// function invocation
+// sayHello();
+
+// variable ()=parameter // Alices
+function printMyName(name) {
+  console.log("The name is: " + name);
+}
+let nameToPrint = "John Doe";
+printMyName(nameToPrint); // arguments
+printMyName("Sandesh");
